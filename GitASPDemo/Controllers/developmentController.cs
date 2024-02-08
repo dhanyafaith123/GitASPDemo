fcc6fc3e7235c62f9b16470d918d730de4b731aa
dhanyafaith123 <dhanya.krishnans@faithinfotech.in>
2024-02-08 15:45:40 +0530

    created devController in development  branch

---
+++ GitASPDemo/Controllers/developmentController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GitASPDemo.Controllers
+{
+    public class developmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            //changes made
+            return View();
+        }
+    }
+}
